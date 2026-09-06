08a049332d2e320558a5e2647173762b34759948
Chabib N. <chabib.nr@gmail.com>
2026-09-06 14:47:15 +0700

    feat: exports pre-process and post-process transformers

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,4 @@
+export { createPreProcessFn } from './transformers/pre-process'
+export { createPostProcessFn } from './transformers/post-process'
 export type { Options } from '#/options'
 export type { JSXAttributeSchema as '~JSXAttributeSchema' } from '#/jsx-attribute-schema.js'
